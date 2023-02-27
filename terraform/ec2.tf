aa8de82e884233e9b2e6b2c866b386e9c5a624aa
ec2-haedal <svvwac98@saltware.co.kr>
2023-02-27 06:08:33 +0000

    보안그룹 추가

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,15 +1,15 @@
 ## Amazon linux2 AMI
 data "aws_ami" "amazon_linux_2" {
   most_recent = true
-  owners      = ["amazon"]
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+  owners = ["amazon"]
 }
 
 #---------------------------------------------------------------
@@ -17,10 +17,10 @@ data "aws_ami" "amazon_linux_2" {
 #---------------------------------------------------------------
 
 ## ansible controller Instance 
-## 생성 후 보안그룹 ssh 여시오
 data "aws_key_pair" "ansible" {
   key_name = "saltware"
 }
+
 resource "aws_instance" "ansible-controller" {
   ami           = data.aws_ami.amazon_linux_2.id
   instance_type = "t3.small"
@@ -32,21 +32,49 @@ resource "aws_instance" "ansible-controller" {
   }
 }
 
-resource "aws_security_group" "ansible" {
-  name_prefix = "ansible-sg"
-  vpc_id      = module.vpc.vpc_id
+resource "aws_security_group" "kubernetes" {
+  name_prefix = "kubernetes-"
+  description = "Security group for Kubernetes cluster"
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["172.20.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = {
-    Name = "ansible-security-group"
+  ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 2379
+    to_port     = 2380
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 10250
+    to_port     = 10255
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group_rule" "kubernetes_egress" {
+  security_group_id = aws_security_group.kubernetes.id
+
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
 
 #---------------------------------------------------------------
 #  인스턴스 생성 3master, 3storage, 2worker
@@ -56,46 +84,49 @@ resource "aws_security_group" "ansible" {
 resource "aws_instance" "master" {
   count = length(module.vpc.private_subnets)
 
-  ami           = data.aws_ami.amazon_linux_2.id
-  instance_type = "t3.small"
-  subnet_id     = module.vpc.private_subnets[count.index]
-  vpc_security_group_ids = [aws_security_group.ansible.id]
+  ami                    = data.aws_ami.amazon_linux_2.id
+  instance_type          = "t3.small"
+  subnet_id              = module.vpc.private_subnets[count.index]
+  vpc_security_group_ids = [aws_security_group.kubernetes.id]
+  key_name               = data.aws_key_pair.ansible.key_name
 
   tags = {
     Name = "master-${count.index}"
   }
 }
 
-## 3 storage instance
-resource "aws_instance" "storage" {
-  count = length(module.vpc.private_subnets)
+# ## 3 storage instance
+# resource "aws_instance" "storage" {
+#   count = length(module.vpc.private_subnets)
 
-  ami           = data.aws_ami.amazon_linux_2.id
-  instance_type = "t3.small"
-  subnet_id     = module.vpc.private_subnets[count.index]
-  vpc_security_group_ids = [aws_security_group.ansible.id]
+#   ami           = data.aws_ami.amazon_linux_2.id
+#   instance_type = "t3.small"
+#   subnet_id     = module.vpc.private_subnets[count.index]
+#   vpc_security_group_ids = [aws_security_group.kubernetes.id]
 
-  tags = {
-    Name = "storage-${count.index}"
-  }
-}
+#   tags = {
+#     Name = "storage-${count.index}"
+#   }
+# }
 
 ## 2 worker instance
 resource "aws_instance" "worker1" {
-  ami           = data.aws_ami.amazon_linux_2.id
-  instance_type = "t3.medium"
-  subnet_id     = module.vpc.private_subnets[0]
-  vpc_security_group_ids = [aws_security_group.ansible.id]
+  ami                    = data.aws_ami.amazon_linux_2.id
+  instance_type          = "t3.medium"
+  subnet_id              = module.vpc.private_subnets[0]
+  vpc_security_group_ids = [aws_security_group.kubernetes.id]
+  key_name               = data.aws_key_pair.ansible.key_name
 
   tags = {
     Name = "worker-0"
   }
 }
 resource "aws_instance" "worker2" {
-  ami           = data.aws_ami.amazon_linux_2.id
-  instance_type = "t3.medium"
-  subnet_id     = module.vpc.private_subnets[1]
-  vpc_security_group_ids = [aws_security_group.ansible.id]
+  ami                    = data.aws_ami.amazon_linux_2.id
+  instance_type          = "t3.medium"
+  subnet_id              = module.vpc.private_subnets[1]
+  vpc_security_group_ids = [aws_security_group.kubernetes.id]
+  key_name               = data.aws_key_pair.ansible.key_name
 
   tags = {
     Name = "worker-1"
